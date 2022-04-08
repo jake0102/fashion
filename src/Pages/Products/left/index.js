8764d9b3c8f03769b9350e292eff31ec20910c4d
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-04-08 11:22:14 +0900

    new

--- src/Pages/Products/left/index.js
+++ src/Pages/Products/left/index.js
@@ -1,16 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+
 function left() {
     return (
         <div className="Products-left">
             <div className="Products-left-top">
                 <div className="Products-left-top-item1">   
                     <div className="item-container">
+                        <div className="item-container-title">T - shirt</div>
 
+                        <div className="item-container-hover">
+                            <div>
+                                <FontAwesomeIcon icon={faEye} />
+                                <span> See more</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div className="Products-left-top-item2">   
                     <div className="item-container">
+                        <div className="item-container-title">Shoes</div>
 
+                        <div className="item-container-hover">
+                            <div>
+                                <FontAwesomeIcon icon={faEye} />
+                                <span> See more</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -18,13 +35,27 @@ function left() {
             <div className="Products-left-bot">
                 <div className="Products-left-bot-item1">   
                     <div className="item-container">
+                        <div className="item-container-title">One piece</div>
 
+                        <div className="item-container-hover">
+                            <div>
+                                <FontAwesomeIcon icon={faEye} />
+                                <span> See more</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div className="Products-left-bot-item2">   
                     <div className="item-container">
+                        <div className="item-container-title">Active wear</div>
 
+                        <div className="item-container-hover">
+                            <div>
+                                <FontAwesomeIcon icon={faEye} />
+                                <span> See more</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Pages/Products/right/index.js
+++ src/Pages/Products/right/index.js
@@ -1,16 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+
 function right() {
     return (
         <div className="Products-right">
             <div className="Products-right-top">
                 <div className="Products-right-top-item1">   
                     <div className="item-container">
+                        <div className="item-container-title">Autumn wear</div>
 
+                        <div className="item-container-hover">
+                            <div>
+                                <FontAwesomeIcon icon={faEye} />
+                                <span> See more</span>
+                            </div>
+                        </div>
                     </div> 
                 </div>
 
                 <div className="Products-right-top-item2">   
                     <div className="item-container">
+                        <div className="item-container-title">Summer wear</div>
 
+                        <div className="item-container-hover">
+                            <div>
+                                <FontAwesomeIcon icon={faEye} />
+                                <span> See more</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -18,13 +35,27 @@ function right() {
             <div className="Products-right-bot">
                 <div className="Products-right-bot-item1">   
                     <div className="item-container">
+                        <div className="item-container-title">Accessories</div>
 
+                        <div className="item-container-hover">
+                            <div>
+                                <FontAwesomeIcon icon={faEye} />
+                                <span> See more</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div className="Products-right-bot-item2">   
                     <div className="item-container">
+                        <div className="item-container-title">Sale shoes</div>
 
+                        <div className="item-container-hover">
+                            <div>
+                                <FontAwesomeIcon icon={faEye} />
+                                <span> See more</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
