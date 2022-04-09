aa3c1e8ea1d58477328ad66d7844c5055365669c
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-04-09 15:00:09 +0900

    new

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Product />}/>
-      {/* <Route path="/about" element={<About />}/> */}
+      <Route path="/about" element={<About />}/>
     </Routes>
   )
 }
