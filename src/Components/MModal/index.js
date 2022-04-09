d1fcd58c4590de0e802d7acb384fe330bf6483ca
jake0102 <jakejapan0102@gmail.com>
2022-04-09 22:25:15 +0900

    new

--- src/Components/MModal/index.js
+++ src/Components/MModal/index.js
@@ -1,12 +1,25 @@
+import { useContext } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestion, faBars, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons'
+
+// Files
+import { FuncContext } from '../../Pages/Product'
+
 function MModal(props) {
 
     let s
 
     props.show === false ? s = 'none' : s = 'block' 
 
+    const handleShow = useContext(FuncContext)
+
     return (
         <div className="MModal" style={{display: `${s}`}}>
-
+            <div className="MModal-nav">
+                <div className="MModal-nav-bar" onClick={() => handleShow()}>
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
+            </div>
         </div>
     )
 }
