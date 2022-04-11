b0152619252e0e52ba58cfdf12df4896238b855d
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-04-11 18:06:30 +0900

    new

--- src/Components/List/index.js
+++ src/Components/List/index.js
@@ -1,7 +1,21 @@
 function List() {
     return (
         <div className="List">
+            <div className="List-title">
+                T - Shirt
+            </div>
 
+            <div className="List-nav">
+                <div className="List-nav-container">
+                    <div className="List-nav-container-wechoice">
+                        おすすめ
+                    </div>
+
+                    <div className="List-nav-container-fil">
+                        並び替え
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
