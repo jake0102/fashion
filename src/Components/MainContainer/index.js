f2ead53201cde0e9736ea659356cc62441e285ad
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-04-11 10:44:40 +0900

    new

--- src/Components/MainContainer/index.js
+++ src/Components/MainContainer/index.js
@@ -1,26 +1,39 @@
 // React methods
-import { createContext } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 // Files
 import P_Home from '../P_Home'
 import TShirt from '../TShirt'
+import Shoes from '../Shoes'
 
 export const ContentsContext = createContext()
 
-const handleChangeContents = function(value){
-
-    console.log(value);
-}
-
-const contents = {
-    handleChangeContents
-}
-
 function MainContainer() {
+
+    const [content, setContent] = useState(<P_Home />)
+
+    const handleChangeContents = function(value){
+        switch (value) {
+            case 'tshirt':
+                setContent(<TShirt />)
+                break;
+            case 'shoes':
+                setContent(<Shoes />)
+                break;
+            default:
+                console.warn('aaa');
+                break;
+        }
+    }
+    
+    const contents = {
+        handleChangeContents
+    }
+
     return (
         <ContentsContext.Provider value={contents}>
             <div className="MainContainer">
-                <P_Home />
+                {content}
             </div>
         </ContentsContext.Provider>
     )
