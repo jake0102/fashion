91e32bb76f32f8361c75355ba85ca11bcef4a892
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-04-12 08:52:38 +0900

    new

--- src/Components/List/index.js
+++ src/Components/List/index.js
@@ -1,9 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar, faSort, faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useRef, useState } from 'react'
 
 import BackToPHome from '../BackToPHome'
 
 function List(props) {
+
+    const [itemNum, setItemNum] = useState(4)
+
+    const itemRef = useRef()
+
+    useEffect(()=>{
+        for (let index = 0; index < itemNum; index++) {
+            
+            // itemRef.current.innerHTML = <div className="List-container-main-item"></div>
+            
+        }
+    }, [itemNum])
+
     return (
         <div className="List">
             <div className="List-title">
@@ -31,20 +45,8 @@ function List(props) {
                     </div>
                 </div>
 
-                <div className="List-container-main">
-                    <div className="List-container-main-item">
-
-                    </div>
-                    <div className="List-container-main-item">
-
-                    </div>
-                    <div className="List-container-main-item">
-
-                    </div>
-
-                    <div className="List-container-main-item">
-
-                    </div>
+                <div className="List-container-main" ref={itemRef}>
+                    
                 </div>
 
                 <div className="List-container-next">
