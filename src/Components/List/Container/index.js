59138faab513ab6e71d4d2b45d69803e2f5d7bc1
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-04-14 11:42:55 +0900

    new

--- src/Components/List/Container/index.js
+++ src/Components/List/Container/index.js
@@ -1,36 +1,76 @@
+import { useEffect, useRef } from "react"
+
 function Container(props) {
-    switch(props.myNum){
-        case 4:
-            return (
-                <div className="List-container-main">
-                    <div className="List-container-main-item"></div>
-                    <div className="List-container-main-item"></div>
-                    <div className="List-container-main-item"></div>
-                    <div className="List-container-main-item"></div>
-                </div>
-            )
-        case 3:
-            return (
-                <div className="List-container-main">
-                    <div className="List-container-main-item"></div>
-                    <div className="List-container-main-item"></div>
-                    <div className="List-container-main-item"></div>
-                </div>
-            )
-        case 2:
-            return (
-                <div className="List-container-main">
-                    <div className="List-container-main-item"></div>
-                    <div className="List-container-main-item"></div>
-                </div>
-            )
-        case 1:
-            return (
-                <div className="List-container-main">
-                    <div className="List-container-main-item"></div>
-                </div>
-            )
-    }
+
+    const ContainerRef = useRef()
+
+    useEffect(()=>{
+
+        fetch('http://localhost:3000/posts')
+            .then((response) => {
+                return response.json()
+            })
+            .then((myJson) => {
+                const items = myJson.map((item)=>{
+                    return item
+                })
+
+                return items
+            })
+            .then((items) => {
+                const html = items.map(item => (
+                    `<div class="List-container-main-item" key=${item.id}>${item.title}</div>`
+                ))
+
+                ContainerRef.current.innerHTML = html.join('')
+            })
+        
+        // Cleanup function
+        return () => {
+
+        }
+    }, [])
+
+    return (
+        <div className="List-container-main">
+            <div className="myRow" ref={ContainerRef}>
+                
+            </div>
+        </div>
+    )
+
+    // switch(props.myNum){
+    //     case 4:
+    //         return (
+    //             <div className="List-container-main">
+    //                 <div className="List-container-main-item"></div>
+    //                 <div className="List-container-main-item"></div>
+    //                 <div className="List-container-main-item"></div>
+    //                 <div className="List-container-main-item"></div>
+    //             </div>
+    //         )
+    //     case 3:
+    //         return (
+    //             <div className="List-container-main">
+    //                 <div className="List-container-main-item"></div>
+    //                 <div className="List-container-main-item"></div>
+    //                 <div className="List-container-main-item"></div>
+    //             </div>
+    //         )
+    //     case 2:
+    //         return (
+    //             <div className="List-container-main">
+    //                 <div className="List-container-main-item"></div>
+    //                 <div className="List-container-main-item"></div>
+    //             </div>
+    //         )
+    //     case 1:
+    //         return (
+    //             <div className="List-container-main">
+    //                 <div className="List-container-main-item"></div>
+    //             </div>
+    //         )
+    // }
 }
 
 export default Container
--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -1,25 +1,4 @@
-import { useEffect, useState } from "react"
-
 function Contact() {
-
-    useEffect(()=>{
-
-        fetch('http://localhost:3000/posts')
-            .then((response) => {
-                return response.json()
-            })
-            .then((myJson) => {
-                myJson.map((item) => {
-                    console.log(item);
-                })
-            })
-
-        // Cleanup function
-        return () => {
-
-        }
-    })
-
     return (
         <div>
             <h1>Contact page</h1>
