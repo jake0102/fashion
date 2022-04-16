fa02377f32d6b98dd12f2d4569d64a5a2250dc7a
jake0102 <jakejapan0102@gmail.com>
2022-04-16 19:33:14 +0900

    new

---
+++ db.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1604508230015-5a54faf1fa56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        des: "$2.99",
+        rating: "3",
+        recommend: true,
+        search: []
+    },
+]
